302f7070c288a29f6c780aee3cadf3882e585d97
wujian <a42715218@gmail.com>
2026-03-06 00:26:01 +0900

    fix render port

--- app.py
+++ app.py
@@ -126,10 +126,9 @@ def delete_expense(id):
     return redirect("/")
 
 
-if __name__ == "__main__":
-    import os
+import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=port
 # render fix    
